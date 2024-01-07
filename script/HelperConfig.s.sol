a93be9a1936b7b4739a163ef6da3d8af8e9d7607
aizen <tarunsanjayrao@gmail.com>
2024-01-07 12:45:33 +0530

    Raffle

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,65 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+import {VRFCoordinatorV2Mock} from "../lib/chainlink-brownie-contracts/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint256 public constant CHAIN_ID = 11155111;
+    uint96 public constant BASE_FEE = 0.25 ether; //LINK
+    uint96 public constant GAS_PRICE_LINK = 1e9; //1 gwei
+
+    constructor() {
+        if (block.chainid == CHAIN_ID) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreatAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+                subscriptionId: 0,
+                callbackGasLimit: 500000
+            });
+    }
+
+    function getOrCreatAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorV2Mock = new VRFCoordinatorV2Mock(
+            BASE_FEE,
+            GAS_PRICE_LINK
+        );
+        vm.stopBroadcast();
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: address(vrfCoordinatorV2Mock),
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+                subscriptionId: 0, //Lets make our script do that for us
+                callbackGasLimit: 500000
+            });
+    }
+}
